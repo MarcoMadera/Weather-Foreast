075cfd6d6b38728c613090a8730c450fcca8cece
MarcoMadera <me@marcomadera.com>
2021-03-15 01:13:06 -0600

    messy structure

---
+++ pages/components/MiniCard.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { getFormattedDate } from "../../lib/utils";
+
+const MiniCard = ({ data, today, selectedCard, setSelectedCard, i }) => {
+  const [selected, setSelected] = useState(false);
+  useEffect(() => {
+    if (selectedCard === i) {
+      setSelected(true);
+    } else {
+      setSelected(false);
+    }
+  }, [selectedCard]);
+  function handleClick(e) {
+    e.preventDefault();
+    setSelectedCard(i);
+  }
+  return (
+    <button onClick={handleClick}>
+      <p>
+        <strong>{today ? "Today" : getFormattedDate(data.date)}</strong>
+      </p>
+      <img src={`/icons/${data.icon}.png`} alt="" />
+      <p>Humidity</p>
+      <p>{data.humidity}%</p>
+      <style jsx>{`
+        button {
+          border: none;
+          background-color: ${selected ? "rgb(85,150,246)" : "unset"};
+          color: ${selected ? "#ffffff" : "#000000"};
+          width: 180px;
+          height: 200px;
+          border-radius: 10px;
+          text-align: center;
+          padding: 0;
+          margin: 0;
+          cursor: pointer;
+          font: inherit;
+        }
+        img {
+          width: 40px;
+          height: auto;
+        }
+        button:focus {
+          border: none;
+          outline: none;
+        }
+      `}</style>
+    </button>
+  );
+};
+
+export default MiniCard;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,136 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { getFormattedFullDate } from "../lib/utils";
+import Chart from "./components/Chart";
+import DayCards from "./components/DayCards";
+import Input from "./components/Input";
+import MainCard from "./components/MainCard";
 
 export default function Home() {
+  const [search, setSearch] = useState("London");
+  const [cityInfo, setCityInfo] = useState({
+    name: "",
+    country: "",
+    coords: {
+      lat: null,
+      lon: null,
+    },
+  });
+  const [timeStamps, setTimeStamps] = useState([]);
+  const [weather, setWheater] = useState({});
+  const FORECAST_API = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&units=metric&cnt=24&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const ONECALL_API = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityInfo.coords?.lat}&lon=${cityInfo.coords?.lon}&exclude=minutely,alerts&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const [data, setData] = useState([]);
+  const [labels, setLabels] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(0);
+  useEffect(() => {
+    fetch(FORECAST_API)
+      .then((d) => d.json())
+      .then(({ city, list }) => {
+        setCityInfo({
+          name: city.name,
+          country: city.country,
+          coords: {
+            lat: city.coord.lat,
+            lon: city.coord.lon,
+          },
+        });
+        setTimeStamps(
+          list.map(({ dt_txt, weather, wind, main }) => ({
+            date: dt_txt,
+            weather: weather[0].main,
+            icon: weather[0].icon,
+            temp: main.temp,
+            humidity: main.humidity,
+            windSpeed: wind.speed,
+          }))
+        );
+      });
+  }, [search]);
+
+  useEffect(() => {
+    if (cityInfo.name) {
+      fetch(ONECALL_API)
+        .then((d) => d.json())
+        .then((data) => {
+          setWheater(data);
+          setCityInfo((cityInfo) => ({ ...cityInfo, timeZone: data.timezone }));
+        });
+    }
+  }, [cityInfo.name]);
+  useEffect(() => {
+    let temps = [];
+    let timelabels = [];
+    if (weather && selectedCard === 0) {
+      weather?.hourly?.forEach(({ temp, dt }, i) => {
+        if (i % 3 === 0 && i <= 24) {
+          temps.push(temp);
+          timelabels.push(getFormattedFullDate(dt * 1000));
+        }
+      });
+      setData(temps);
+      setLabels(timelabels);
+    } else {
+      timeStamps.forEach(({ temp, date }, i) => {
+        if (i <= selectedCard * 8 && i >= (selectedCard - 1) * 8) {
+          temps.push(temp);
+          timelabels.push(date);
+        }
+      });
+      setData(temps);
+      setLabels(timelabels);
+    }
+  }, [weather, timeStamps, selectedCard]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+    <div>
+      <main>
+        <Head>
+          <title>Weather Forecast</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Input setSearch={setSearch} />
+        <MainCard
+          cityInfo={cityInfo}
+          weather={weather}
+          selectedCard={selectedCard}
+        />
+        <Chart data={data} labels={labels} />
+        {weather && (
+          <DayCards
+            weather={weather}
+            setSelectedCard={setSelectedCard}
+            selectedCard={selectedCard}
+          />
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <style jsx>
+        {`
+          div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 50px 64px;
+            width: 100vw;
+            height: 100vh;
+          }
+          main {
+            height: 100%;
+            align-items: center;
+            justify-content: center;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-template-rows: 50px 200px 1fr;
+            grid-template-areas: "input ." "mainCard chart" "mainCard dayCards";
+            padding: 50px 35px;
+            width: 100%;
+            height: 100%;
+            box-shadow: rgb(225, 237, 255) 4px 4px 0 0;
+            border-radius: 10px;
+            background-color: white;
+          }
+        `}
+      </style>
     </div>
-  )
+  );
 }
